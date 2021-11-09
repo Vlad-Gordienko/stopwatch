939d4a0dc5f37d059d259347ef90ddffb4d91557
voland <voland.gordienko@gmail.com>
2021-11-09 15:34:21 +0200

    refactor

--- src/app/stopwatch/stopwatch.component.ts
+++ src/app/stopwatch/stopwatch.component.ts
@@ -1,30 +1,35 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs/internal/Subscription';
 import { interval } from 'rxjs';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-stopwatch',
   templateUrl: './stopwatch.component.html',
   styleUrls: ['./stopwatch.component.css']
 })
-export class StopwatchComponent implements OnInit, OnDestroy {
-  public time: Date = new Date(1970, 0,1);
+export class StopwatchComponent implements OnDestroy {
+  public time: Date = this.initialTime;
   private intervalSubscriber: Subscription = new Subscription;
   private intervalStep: number = 83;
   public isRun: boolean = false;
-
-  public ngOnInit(): void {
-  }
 
   public ngOnDestroy(): void {
     this.intervalSubscriber.unsubscribe();
   }
 
+  public get initialTime(): Date {
+    return new Date(1970, 0,1);
+  }
 
   public start(): void {
-    this.isRun = true;
     this.intervalSubscriber.unsubscribe();
+
+    this.isRun = true;
     this.intervalSubscriber = interval(this.intervalStep)
+      .pipe(
+        finalize(() => this.isRun = false)
+      )
       .subscribe(() => {
         const newDateTimestamp = this.time.getTime() + this.intervalStep;
         this.time = new Date(newDateTimestamp);
@@ -33,12 +38,10 @@ export class StopwatchComponent implements OnInit, OnDestroy {
 
   public pause(): void {
     this.intervalSubscriber.unsubscribe();
-    this.isRun = false;
   }
 
   public stop(): void {
     this.intervalSubscriber.unsubscribe();
-    this.time = new Date(1970, 0,1);
-    this.isRun = false;
+    this.time = this.initialTime;
   }
 }
